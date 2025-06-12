da5cebf689f047a6f76e043d25db9515fd18dae8
Joshua Zhou <joshuazhou0808@gmail.com>
2025-06-11 22:40:01 -0700

    Write Determining Attitude Case For Attitude Control

--- include/Config.hpp
+++ include/Config.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "GrotiferMaster.hpp"
 struct AttitudeConfig
 {
     static inline constexpr double deltaTaskTime = 75e-3;
@@ -16,4 +17,60 @@ struct AttitudeConfig
     // Z-momentum wheel
     double momOfInertiaZ = 3.6383e-5; // Moment of inertia for Z, [kg.m^2]
     long int maxVelZ = 4600;          // Maximum speed for Z-momentum wheel, [rpm]
+
+    double fc = 4.0; // Cut off frequency for angular velocity filtering
 };
+// --- Maxon motor factory functions ---
+inline maxon makeXMotor()
+{
+    maxon m;
+    m.nameMotor = "xMomMotor";
+    m.serialNo = 0x40008356;
+    m.K_P = 259266;
+    m.K_I = 2450065;
+    m.KFF_VEL = 5306;
+    m.KFF_ACC = 6859;
+    m.K_TUNING = 1;
+    m.KP_CURR = 2000035;
+    m.KI_CURR = 3362724;
+    m.KT = 24.6e-3;
+    m.NUM_OF_PULSE_PER_REV = 512;
+    m.SENSOR_POLARITY = 0;
+    return m;
+}
+
+inline maxon makeYMotor()
+{
+    maxon m;
+    m.nameMotor = "yMomMotor";
+    m.serialNo = 0x40008457;
+    m.K_P = 260961;
+    m.K_I = 2466086;
+    m.KFF_VEL = 4699;
+    m.KFF_ACC = 6904;
+    m.K_TUNING = 1;
+    m.KP_CURR = 1822756;
+    m.KI_CURR = 2513998;
+    m.KT = 24.6e-3;
+    m.NUM_OF_PULSE_PER_REV = 512;
+    m.SENSOR_POLARITY = 0;
+    return m;
+}
+
+inline maxon makeZMotor()
+{
+    maxon m;
+    m.nameMotor = "zMomMotor";
+    m.serialNo = 0x40008446;
+    m.K_P = 266273;
+    m.K_I = 2516281;
+    m.KFF_VEL = 6600;
+    m.KFF_ACC = 7044;
+    m.K_TUNING = 1;
+    m.KP_CURR = 1789845;
+    m.KI_CURR = 2370753;
+    m.KT = 24.6e-3;
+    m.NUM_OF_PULSE_PER_REV = 512;
+    m.SENSOR_POLARITY = 0;
+    return m;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
-﻿int main()
+﻿#include "BaseTask.hpp"
+#include "Tasks.hpp"
+#include "Config.hpp"
+
+#define SUNSENSOR_SERIAL_PORT "/dev/digital_sun_sensor"
+
+int main()
 {
 }
