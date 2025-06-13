463deb32152d8f61e781bd91a0f5ac81585ffa0d
Orion V <orionv@berkeley.edu>
2025-06-13 00:43:44 -0700

    added devices.cpp, devices.hpp, edit to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 ﻿#include "BaseTask.hpp"
 #include "Tasks.hpp"
 #include "Config.hpp"
+#include "Devices.hpp"
 
 #define SUNSENSOR_SERIAL_PORT "/dev/digital_sun_sensor"
 
 int main()
 {
+    Devices devices;
+
+    // Now we can access devices here
+    // For example:
+    double angleX = devices.sunSensor.GetAngleX();
+    // or
+    double voltage = devices.grotiferLJU6.GetRawVoltageAtChannel(0);
+
+    
 }
