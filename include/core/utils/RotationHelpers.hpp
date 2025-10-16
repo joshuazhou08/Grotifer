6d3b7199daad26020c0a09d0419fb80c3639c729
Joshua Zhou <joshuazhou0808@gmail.com>
2025-10-16 11:48:20 -0700

    Move Software Utility Functions

--- include/core/utils/RotationHelpers.hpp
+++ include/core/utils/RotationHelpers.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <Eigen/Dense>
+#include <cmath>
 
 namespace RotationHelpers {
     // Compute incremental rotation matrix between two body orientations.
@@ -7,5 +8,15 @@ namespace RotationHelpers {
                                       const Eigen::Matrix3d& curRotMat)
     {
         return curRotMat * prevRotMat.transpose();
+    }
+
+    // Convert angle from degrees to radians
+    inline double Deg2Rad(double angleDeg) {
+        return angleDeg * M_PI / 180.0;
+    }
+
+    // Convert angle from radians to degrees
+    inline double Rad2Deg(double angleRad) {
+        return angleRad * 180.0 / M_PI;
     }
 }
