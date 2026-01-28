4f304c6e093a55370fcf4a44dcbefa35e814ee77
Joshua Zhou <joshuazhou@berkeley.edu>
2026-01-28 12:03:37 -0800

    Delete Unused

--- src/core/Config.cpp
+++
@@ -1,9 +0,0 @@
-#include "Config.hpp"
-#include "core/utils/RotationHelpers.hpp"
-
-std::vector<RotationCommand> AttitudeConfig::getRotationQueue()
-{
-    return {
-        RotationCommand(Vector3d{0.0, 0.0, 1.0}, M_PI / 30.0, vel, acc),
-    };
-}
